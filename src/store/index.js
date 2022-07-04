1b422d4df2c04aec29a104e108c7382da6cb9ce1
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-04 15:25:58 +0500

    feat(index): coonect redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { cashReducer } from './cashReduser';
 import { customerReducer } from './customerReducer';
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 const rootReducers = combineReducers({
   cash: cashReducer,
   customers: customerReducer,
 });
 
-export const store = createStore(rootReducers, composeWithDevTools());
+export const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)));
